36361acde44939d44c103b22012f5355f98af50f
Faiz Mokhtar <mfmokhtar@gmail.com>
2022-04-05 07:51:35 +0800

    Initial Commit

---
+++ JurnalKopi/JurnalKopiApp.swift
@@ -0,0 +1,17 @@
+//
+//  JurnalKopiApp.swift
+//  JurnalKopi
+//
+//  Created by AD0502-ADE-MB-1 on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct JurnalKopiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
